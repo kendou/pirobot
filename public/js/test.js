bb6cef15ebc20e2e6daceb125f550f65ce7790c6
kendou <kenthuang72@gmail.com>
2015-08-03 00:08:51 +0800

    test for the Led function

---
+++ public/js/test.js
@@ -0,0 +1,50 @@
+/**
+ * Created by huangjian on 15/8/2.
+ */
+
+var socket = new io.connect({
+  'reconnection': true,
+  'reconnectionDelay': 1000,
+  'reconnectionDelayMax' : 5000,
+  'reconnectionAttempts': 5
+});
+
+socket.on('ledMessage', function(ledMap) {
+  console.log(ledMap);
+});
+
+socket.on('connect', function(){
+  console.log("Connected.");
+});
+
+socket.on('connect_error', function(error){
+  console.log("Connection error: " + error.toString());
+});
+
+socket.on('connect_timeout', function(){
+  console.log("Connect timeout.")
+});
+
+socket.on('reconnect', function(reconnectNum){
+  console.log("Reconnected after " + reconnectNum + " retries.");
+});
+
+socket.on('reconnecting', function(reconnectNum){
+  console.log("Reconnection attempt number:" + reconnectNum);
+});
+
+socket.on('reconnect_error', function(error){
+  console.log("Reconnection error: " + error.toString());
+});
+
+socket.on('reconnect_failed', function(){
+  console.log("Reconnection failed.");
+});
+
+function toggleLed1() {
+  socket.emit('toggleLed', {led: 'led1'});
+}
+
+function toggleLed2() {
+  socket.emit('toggleLed', {led: 'led2'});
+}
